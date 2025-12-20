6a87928bb28874928ebcf2d2672a483226feaf33
polarpia-tech <polarpia@gmail.com>
2025-12-20 17:26:03 +0100

    Update cache version to mini-cashier-cache-v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mini-cashier-cache-v3";
+const CACHE = "mini-cashier-cache-v4";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
 
 self.addEventListener("install", (e) => {
