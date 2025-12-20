44bee2dab10c41aacb0f301d1e23f516fa081bdc
polarpia-tech <polarpia@gmail.com>
2025-12-20 16:56:09 +0100

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = "mini-cashier-cache-v3";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).then((resp) => {
+      const copy = resp.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(()=>cached))
+  );
+});
